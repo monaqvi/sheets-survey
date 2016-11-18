2f4558f0667b5556a8702fe84652bb197d4c5e9a
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-11-17 16:26:14 -0800

    Add tabordion validation

--- client/assets/main.js
+++ client/assets/main.js
@@ -1,28 +1,49 @@
 $(function() {
+  var dropdown = 'select',
+      input = 'textarea, input';
+
 // First validate non-tabordion answers
+  var required = $('form > fieldset[required]');
 
   // Validate required dropdowns
-  $('form > fieldset[required] > select').filter(function(i, e) {
-    return e.value === 'select'
-  });
+  required.children(dropdown).filter(invalidDropdown);
 
   // Validate required inputs
-  $('form > fieldset[required] > input').filter(function(i, e) {
-    return e.value === ''
-  });
+  required.children(input).filter(invalidInput);
 
 // Now validate tabordion answers
 
   // If never checked, disable all other checkboxes
-  $('form > .tabordion .checkbox > input#never').click(function() {
-    $('form > .tabordion .checkbox > input').not('#never')
+  var tabordion = $('form > .tabordion input[type = checkbox]');
+
+  tabordion.filter('#never').click(function() {
+    tabordion.not('#never')
       .prop('disabled', this.checked)
       .prop('checked', false);
   });
 
   // Make sure at least one checkbox is selected
+  var active_tabordion = tabordion.filter(':checked');
+
+  // Validate required dropdowns within checked tabordion
+  var required_tabordion = active_tabordion.siblings().children('fieldset[required]');
+
+  // Validate required dropdowns
+  required.children(dropdown).filter(invalidDropdown);
+
+  // Validate required inputs
+  required.children(input).filter(invalidInput);
 
   $('#submit').click(function() {
     console.log('validate and save!');
   });
+
+  function invalidDropdown(i, e) {
+    return e.value === 'select';
+  }
+
+  function invalidInput(i, e) {
+    return e.value === '';
+  }
+
 });
