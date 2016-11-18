a59c024ec1db6330e99e14e4b21573c0bd02a78a
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-11-17 17:45:48 -0800

    Finish form validation

--- client/assets/main.js
+++ client/assets/main.js
@@ -1,49 +1,65 @@
 $(function() {
-  var dropdown = 'select',
-      input = 'textarea, input';
-
-// First validate non-tabordion answers
-  var required = $('form > fieldset[required]');
-
-  // Validate required dropdowns
-  required.children(dropdown).filter(invalidDropdown);
-
-  // Validate required inputs
-  required.children(input).filter(invalidInput);
-
-// Now validate tabordion answers
-
-  // If never checked, disable all other checkboxes
-  var tabordion = $('form > .tabordion input[type = checkbox]');
-
-  tabordion.filter('#never').click(function() {
-    tabordion.not('#never')
-      .prop('disabled', this.checked)
-      .prop('checked', false);
-  });
-
-  // Make sure at least one checkbox is selected
-  var active_tabordion = tabordion.filter(':checked');
-
-  // Validate required dropdowns within checked tabordion
-  var required_tabordion = active_tabordion.siblings().children('fieldset[required]');
-
-  // Validate required dropdowns
-  required.children(dropdown).filter(invalidDropdown);
-
-  // Validate required inputs
-  required.children(input).filter(invalidInput);
 
   $('#submit').click(function() {
-    console.log('validate and save!');
+    var invalid = validateForm();
+
+    $('.invalid').removeClass('invalid');
+    $(invalid).parent().addClass('invalid');
   });
 
-  function invalidDropdown(i, e) {
-    return e.value === 'select';
-  }
+  function validateForm() {
+    var invalid = [];
 
-  function invalidInput(i, e) {
-    return e.value === '';
-  }
+  // First validate non-tabordion answers
+    var required = $('form > fieldset[required]');
 
+    var invalid = validateNode(required);
+
+  // Now validate tabordion answers
+
+    // If never checked, disable all other checkboxes
+    var tabordion = $('form > .tabordion input[type = checkbox]');
+
+    tabordion.filter('#never').click(function() {
+      tabordion.not('#never')
+        .prop('disabled', this.checked)
+        .prop('checked', false);
+    });
+
+    // Make sure at least one checkbox is selected
+    var activeTabordion = tabordion.filter(':checked');
+
+    // Validate required dropdowns within checked tabordion
+    var requiredTabordion = activeTabordion.siblings().children('fieldset[required]');
+
+    var invalidTabordion = validateNode(requiredTabordion);
+
+    console.log($(invalid).parent());
+
+    return (activeTabordion.length ? [] : tabordion.toArray())
+            .concat(invalid)
+            .concat(invalidTabordion);
+
+    function validateNode(node) {
+      var dropdown = 'select',
+          input = 'textarea, input';
+
+      // Validate required dropdowns
+      var invalidDropdowns = node.children(dropdown).filter(invalidDropdown);
+
+      // Validate required inputs
+      var invalidInputs = node.children(input).filter(invalidInput);
+
+      return invalidDropdowns.toArray()
+              .concat(invalidInputs.toArray());
+    }
+
+    function invalidDropdown(i, e) {
+      return e.value === 'select';
+    }
+
+    function invalidInput(i, e) {
+      return e.value === '';
+    }
+  }
 });
